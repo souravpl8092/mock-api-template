6be0f6e9b8919a019769cd0782af3b18c149bf04
Sourav Paul <souravpl8092@gmail.com>
2023-02-09 12:43:30 +0530

    update

--- server.js
+++ server.js
@@ -2,9 +2,8 @@ const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = 3001;
 
 server.use(middlewares);
 server.use(router);
 
-server.listen(port);
+server.listen(3001);
